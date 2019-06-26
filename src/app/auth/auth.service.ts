10e392aac217ece5bd311428208feb9b195f9870
unknown <l.biniszewski@microsoft.wsei.edu.pl>
2019-06-26 21:22:32 +0200

    user verification aplication state

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,41 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
 
-  constructor(private angularFire: AngularFireAuth) { }
+  user: User; //jest to token, użytkownik który jest zawracany po zalogowaniu
+
+  constructor(private angularFire: AngularFireAuth, private router: Router) { 
+    //metoda sprawdzająca użytkownika, będzie informowała czy użytkownik jest zalogowany czy nie jest
+    // metoda zwraca observable którym jest użytkownik albo null
+    //jeżeli pojawi się użytkownik to poprzez ta metode zo zmiennej user zostanie dopisany użytkownik
+    //analiza czy jest null czy jest użytkownik wykorzystana zostanie do kontroli widoków zalogowany/niezalogowany
+    //metoda będzie odpalana zawsze gdy po stronie firebase zmieni sie stan tokena
+    //metoda działa automatycznie ponieważ ją subskrybujemy
+    angularFire.authState.subscribe(user=>{
+      this.user = user;
+    });
+  }
 
   login(email: string, password: string){
     this.angularFire.auth.signInWithEmailAndPassword(email, password)
-    .then(value=>{
-      console.log(value);
+    .then(user=>{
+      this.router.navigate(['/home']);
     }).catch(err=>{
       console.log(err);
     });
   }
 
   signup(email: string, password: string){
-    this.angularFire.auth.createUserWithEmailAndPassword(email,password).then(value => {
-      console.log(value);
+    this.angularFire.auth.createUserWithEmailAndPassword(email,password).then(user => {
+      console.log(user);
     }).catch(err => {
       console.log(err);
     });
