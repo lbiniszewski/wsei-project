de7407da1471a971d1658db28ff2b328d6a2fa47
adasko111 <adam.blady4@gmail.com>
2019-06-27 22:10:17 +0200

    Profile component added to main site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,17 @@ import { NavbarModule, WavesModule, ButtonsModule, InputsModule } from 'angular-
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppSearchComponent } from './search/app-search.component';
-
+import { ReadonlyprofileComponent } from './profile/readonlyprofile/readonlyprofile.component';
+import { ShowProfileEngineComponent } from './profile/readonlyprofile/show-profile-engine/show-profile-engine.component';
+import { ShowPhotoComponent } from './profile/readonlyprofile/show-photo/show-photo.component';
+import { ShowNameComponent } from './profile/readonlyprofile/show-name/show-name.component';
+import { ShowOpinionComponent } from './profile/readonlyprofile/show-opinion/show-opinion.component';
+import { ShowTematicalModuleComponent } from './profile/readonlyprofile/show-tematical-module/show-tematical-module.component';
+import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
+import { EditProfileEngineComponent } from './profile/edit-profile/edit-profile-engine/edit-profile-engine.component';
+import { TextFieldComponent } from './profile/edit-profile/text-field/text-field.component';
+import { AddButtonComponent } from './profile/edit-profile/add-button/add-button.component';
+import { CancleButtonComponent } from './profile/edit-profile/cancle-button/cancle-button.component';
 
 
 const firebaseConfig = {
@@ -50,6 +60,17 @@ const firebaseConfig = {
     ReactiveFormComponent,
     LoginComponent,
     AppSearchComponent,
+    ReadonlyprofileComponent,
+    ShowProfileEngineComponent,
+    ShowPhotoComponent,
+    ShowNameComponent,
+    ShowOpinionComponent,
+    ShowTematicalModuleComponent,
+    EditProfileComponent,
+    EditProfileEngineComponent,
+    TextFieldComponent,
+    AddButtonComponent,
+    CancleButtonComponent
   ],
   imports: [
     BrowserModule, 
